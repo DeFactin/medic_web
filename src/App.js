c89df254b97e2d4dbb4e874b9b2b664c212ab524
DeFactin <aminadefacto47@gmail.com>
2024-08-01 15:09:54 +0200

    Routes Change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/navbar'
 import Login from './pages/Login'
-import Home
-  from './pages/Home'
+import Home from './pages/Home'
 import './App.css'
+import { useAuthContext } from './hooks/useAuthContext'
+
 
 function App() {
+
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,11 +17,11 @@ function App() {
         <Routes>
           <Route
             path="/home"
-            element={<Home />}
+            element={user ? <Home /> : <Navigate to="/" />}
           />
           <Route
             path="/"
-            element={<Login />}
+            element={!user ? <Login /> : <Navigate to="/home" />}
           />
         </Routes>
       </BrowserRouter>
